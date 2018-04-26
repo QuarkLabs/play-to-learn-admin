18745e6dc6a54ae9aae3558ba0439848b8e2f160
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-04-27 04:39:14 +0530

    Create firebase service

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.scss']
+  styleUrls: ['./questions.component.scss'],
+  providers: [FirebaseService]
 })
 export class QuestionsComponent implements OnInit {
 
   public csvText: '';
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
   // Add Questions
   onClickAddQuestionsButton(){
