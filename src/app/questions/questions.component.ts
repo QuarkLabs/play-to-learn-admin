6aabf08e2a202f0300523a855e91230397b778d0
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-04-30 02:44:17 +0530

    Add new levels button

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -21,18 +21,20 @@ export class QuestionsComponent implements OnInit {
     this.title = "Questions";
 
     // Load questions
-    this.mapQuestions();
+    this.loadQuestions();
 
+    // Get levels
     this.db.list('levels').snapshotChanges(['child_added'])
     .subscribe(actions => {
       actions.forEach(action => {
-        this.levels.push(action.key);
-        console.log(action.key);
+        if (this.levels.indexOf(action.key) == -1){
+          this.levels.push(action.key);
+        }
       });
     });
   }
 
-  mapQuestions(){
+  loadQuestions(){
     // Use snapshotChanges().map() to store the key
     this.questions = this.getQuestionList().snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
@@ -45,7 +47,11 @@ export class QuestionsComponent implements OnInit {
       description: "",
       answer: "0",
       correctPoints: "10",
-      wrongPoints: "-5"
+      wrongPoints: "-5",
+      category: { 
+        id: "conditional_structures",   // TODO: Category Selector
+        name: "Conditional Structures"  // TODO: Category Selector
+      }
     });
   }
 
@@ -70,6 +76,18 @@ export class QuestionsComponent implements OnInit {
   
   getQuestionList(){
     return this.db.list('levels/' + this.level + '/questions');
+  }
+
+  addNewLevel(){
+    var newNum: number;
+    if (this.levels.length == 0){
+      newNum = 1;
+    } else {
+      var newNum = parseInt(this.levels[this.levels.length - 1]) + 1;
+    }
+    this.levels.push(newNum.toString());
+    this.level = newNum.toString();
+    this.loadQuestions();
   }
 
 
